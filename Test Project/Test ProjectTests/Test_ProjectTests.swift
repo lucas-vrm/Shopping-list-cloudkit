4d8439bfcfdd2e4ae1f2711c718ce0a3fbbbd095
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-30 09:03:01 -0300

    Add Xcode project

---
+++ Test Project/Test ProjectTests/Test_ProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_ProjectTests.swift
+//  Test ProjectTests
+//
+//  Created by Karen Lima on 30/09/21.
+//
+
+import XCTest
+@testable import Test_Project
+
+class Test_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
